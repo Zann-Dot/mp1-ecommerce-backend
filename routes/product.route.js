28c89e8f572d7b5aafebb8b9c1bfb64d80ce125f
Zann-Dot <anaykarn234@gmail.com>
2026-05-15 21:43:16 +0530

    feat: implement product retrieval routes for list, ID, category, and rating filters

--- routes/product.route.js
+++ routes/product.route.js
@@ -27,4 +27,33 @@ router.get("/products/:productId", async (req, res) => {
   }
 });
 
+router.get("/products/category/:category", async (req, res) => {
+  try {
+    const productsByCategory = await Products.find({
+      category: req.params.category,
+    });
+
+    if (productsByCategory.length === 0)
+      return res.status(404).json({ error: "Products not found" });
+
+    res.json(productsByCategory);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+router.get("/products/ratings/:ratings", async (req, res) => {
+  try {
+    const ratings = parseInt(req.params.ratings);
+    if (ratings >= 10) {
+      const products = await Products.find({
+        ratings: { $gte: ratings, $lt: ratings + 10 },
+      });
+      return res.json(products);
+    }
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
 export default router;
