5ca4ba599980ce0004f13d6bb8ea2012370828c9
Zann-Dot <anaykarn234@gmail.com>
2026-06-06 21:45:28 +0530

    refactor: access min and max price from query params and added condition filtering in query priceRupees.

--- routes/product.route.js
+++ routes/product.route.js
@@ -3,70 +3,72 @@ import Products from "../models/products.model.js";
 const router = express.Router();
 
 router.get("/products", async (req, res) => {
-  try {
-    const categoryQuery = req.query.c;
-    let query = {}
+    try {
+        const categoryQuery = req.query.c;
+        const minPrice = req.query.min;
+        const maxPrice = req.query.max;
+        let query = {};
 
-    if (categoryQuery) {
-      const category = Array.isArray(categoryQuery) ? categoryQuery : [categoryQuery];
-      query.category = { $in: category };
+        if (categoryQuery) {
+            const categoryArray = Array.isArray(categoryQuery)
+                ? categoryQuery
+                : [categoryQuery];
+            query.category = { $in: categoryArray };
+        }
+
+        if (minPrice && maxPrice)
+            query.priceRupees = { $gte: Number(minPrice), $lte: Number(maxPrice) };
+
+        const products = await Products.find(query);
+
+        if (products.length === 0)
+            return res.status(404).json({ error: "Products not found" });
+
+        res.json(products);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
     }
-
-    const products = await Products.find(query);
-
-    if (products.length === 0)
-      return res.status(404).json({ error: "Products not found" });
-
-    res.json(products);
-
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
 });
 
 router.get("/products/:productId", async (req, res) => {
-  try {
-    const product = await Products.findById(req.params.productId);
+    try {
+        const product = await Products.findById(req.params.productId);
 
-    if (!product) return res.status(404).json({ error: "Product not found" });
+        if (!product) return res.status(404).json({ error: "Product not found" });
 
-    res.json(product);
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
+        res.json(product);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
 });
 
-
 router.get("/products/ratings/:ratings", async (req, res) => {
-  try {
-    const ratings = parseInt(req.params.ratings);
-    if (ratings >= 10) {
-      const products = await Products.find({
-        ratings: { $gte: ratings, $lt: ratings + 10 },
-      });
-      return res.json(products);
+    try {
+        const ratings = parseInt(req.params.ratings);
+        if (ratings >= 10) {
+            const products = await Products.find({
+                ratings: { $gte: ratings, $lt: ratings + 10 },
+            });
+            return res.json(products);
+        }
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
     }
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
 });
 
 router.get("/products/search/:search", async (req, res) => {
-  try {
-    const { search } = req.params;
+    try {
+        const { search } = req.params;
 
-    const searchRegex = new RegExp(`\\b${search}\\b`, 'i');
-    const searchedProducts = await Products.find({
-      $or: [
-        { productName: searchRegex },
-        { category: searchRegex }
-      ]
-    });
+        const searchRegex = new RegExp(`\\b${search}\\b`, "i");
+        const searchedProducts = await Products.find({
+            $or: [{ productName: searchRegex }, { category: searchRegex }],
+        });
 
-    res.json(searchedProducts);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+        res.json(searchedProducts);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
 export default router;
